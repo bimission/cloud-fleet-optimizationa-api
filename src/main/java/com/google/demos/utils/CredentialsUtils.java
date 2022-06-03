0be0e7cacfc5970d9e1a0a2382fdbd12f2ab4015
Łukasz Olejniczak <lolejniczak@google.com>
2022-06-03 09:29:44 +0200

    init

--- src/main/java/com/google/demos/utils/CredentialsUtils.java
+++ src/main/java/com/google/demos/utils/CredentialsUtils.java
@@ -2,5 +2,5 @@ package com.google.demos.utils;
 
 public class CredentialsUtils {
 
-    public static String accessToken = "AIzaSyDJYQKz1oxGN5CG64ImUHz_5FZQwJuTYCs";
+    public static String accessToken = "XYZ";
 }
